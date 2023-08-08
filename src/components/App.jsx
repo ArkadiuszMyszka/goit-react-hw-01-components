0dffdd642c237499e4d5c63a7950afaf54465ca2
Arkadiusz Myszka <lokonhinio@gmail.com>
2023-08-08 21:15:00 +0200

    Transactions done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Profile from './Profile';
 import Statistics from './Statistics';
 import FriendList from './FriendsList';
+import Transactions from './Transactions';
 
 import user from '../data/user';
 import stats from '../data/statistics';
 import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -18,7 +20,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={stats} />
       <FriendList friends={friends} />
-      {/* <TransactionHistory items={transactions} /> */}
+      <Transactions items={transactions} />
     </div>
   );
 };
---
+++ src/components/Transactions/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Transactions.module.css';
+
+const Transactions = ({ items }) => {
+
+
+  return (
+    <table role="table" className={`${styles.container} `}>
+      <thead>
+        <tr className={styles.row}>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items && typeof items === 'object'
+          ? items.map(el => (
+              <tr key={el.id} className={styles.row}>
+                <td role="cell">{el.type}</td>
+                <td>{el.amount}</td>
+                <td>{el.currency}</td>
+              </tr>
+            ))
+          : null}
+      </tbody>
+    </table>
+  );
+};
+
+Transactions.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      type: PropTypes.string,
+      amount: PropTypes.string,
+      currency: PropTypes.string,
+    })
+  ).isRequired,
+};
+
+export default Transactions;
